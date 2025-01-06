a48216927b3bd1d88d631c2304e0f1d67f431454
ericmullo <ericrodrigomullo@gmail.com>
2025-01-06 08:00:27 -0500

    realize modificaciones dependiendo de lo que se pedia

--- People/MauiProgram.cs
+++ People/MauiProgram.cs
@@ -1,26 +1,25 @@
 ﻿using Microsoft.Extensions.Logging;
 
-namespace People;
-
-public static class MauiProgram
+namespace People
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
 
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
+            // Agregamos el código para configurar el repositorio
+            string dbPath = FileAccessHelper.GetLocalFilePath("people.db3");
+            builder.Services.AddSingleton<PersonRepository>(s => ActivatorUtilities.CreateInstance<PersonRepository>(s, dbPath));
 
-		// TODO: Add statements for adding PersonRepository as a singleton
-
-		return builder.Build();
-	}
+            return builder.Build();
+        }
+    }
 }
--- People/PersonRepository.cs
+++ People/PersonRepository.cs
@@ -1,60 +1,69 @@
 ﻿using People.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
 
-namespace People;
-
-public class PersonRepository
+namespace People
 {
-    string _dbPath;
-
-    public string StatusMessage { get; set; }
-
-    // TODO: Add variable for the SQLite connection
-
-    private void Init()
+    public class PersonRepository
     {
-        // TODO: Add code to initialize the repository         
-    }
+        private string _dbPath; // Declaración correcta del campo privado
+        private SQLiteConnection conn; // Declaración de la conexión dentro de la clase
 
-    public PersonRepository(string dbPath)
-    {
-        _dbPath = dbPath;                        
-    }
+        public string StatusMessage { get; set; }
 
-    public void AddNewPerson(string name)
-    {            
-        int result = 0;
-        try
+        public PersonRepository(string dbPath)
         {
-            // TODO: Call Init()
-
-            // basic validation to ensure a name was entered
-            if (string.IsNullOrEmpty(name))
-                throw new Exception("Valid name required");
-
-            // TODO: Insert the new person into the database
-            result = 0;
-
-            StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
-        }
-        catch (Exception ex)
-        {
-            StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
+            _dbPath = dbPath; // Inicializa la ruta de la base de datos
         }
 
-    }
+        // Método para inicializar la conexión SQLite
+        private void Init()
+        {
+            if (conn != null)
+                return; // Si la conexión ya está inicializada, salimos del método
 
-    public List<Person> GetAllPeople()
-    {
-        // TODO: Init then retrieve a list of Person objects from the database into a list
-        try
-        {
-            
-        }
-        catch (Exception ex)
-        {
-            StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            conn = new SQLiteConnection(_dbPath); // Inicializamos la conexión
+            conn.CreateTable<Person>(); // Creamos la tabla Person si no existe
         }
 
-        return new List<Person>();
+        // Método para agregar una nueva persona
+        public void AddNewPerson(string name)
+        {
+            int result = 0;
+            try
+            {
+                Init(); // Aseguramos que la conexión esté inicializada
+
+                // Validación básica para asegurarse de que se ha ingresado un nombre
+                if (string.IsNullOrEmpty(name))
+                    throw new Exception("Valid name required");
+
+                // Insertamos la nueva persona en la base de datos
+                result = conn.Insert(new Person { Name = name });
+
+                StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
+            }
+        }
+
+        // Método para obtener todas las personas
+        public List<Person> GetAllPeople()
+        {
+            try
+            {
+                Init(); // Aseguramos que la conexión esté inicializada
+                return conn.Table<Person>().ToList(); // Retornamos la lista de personas
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            return new List<Person>(); // Retornamos una lista vacía en caso de error
+        }
     }
 }
